cd52d09c07b8d4471de9a0e3df3b106f315feee9
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-26 15:09:08 -0300

    Utilizando separador

---
+++ logic_prog/aula2.py
@@ -0,0 +1,10 @@
+# print(123456)
+# print('Lucas', 'Henrique')
+
+"""
+print('Lucas', 'Henrique', sep='-', end='###')
+print('Joao', 'e', 'Maria', sep='-', end='')
+"""
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
