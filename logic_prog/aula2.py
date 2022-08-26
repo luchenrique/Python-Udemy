1331a3bb5439757fbce8d686a2fffa03c5bc255e
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-26 15:17:00 -0300

    Lógica de Programação

--- logic_prog/aula2.py
+++ logic_prog/aula2.py
@@ -5,6 +5,12 @@
 print('Lucas', 'Henrique', sep='-', end='###')
 print('Joao', 'e', 'Maria', sep='-', end='')
 """
-
+"""
 print('824', '176', '070', sep='.', end='-')
 print('18')
+"""
+
+### Pergunta 1: Qual a saída do comando abaixo:
+
+print('Estou', 'aprendendo', 'Python', sep='-', end=' ')
+print('isso é muito', 'legal', sep='-', end='')
