542d506bfc059e0852276dd2bab951a79205d1ae
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-28 15:48:54 -0300

    resolvido

--- logic_prog/aula4/aula4.py
+++ logic_prog/aula4/aula4.py
@@ -17,6 +17,32 @@ print('L' == 'l', type('L' == 'l'))  #bool-false
 # String - Nome
 print('Lucas Henrique', type('Lucas Henrique'))
 
+# Int - Idade.
+print(19, type(19))
+
+# Float - Altura
+print(1.83, type(1.83))
+
+# Bool - Maior de idade
+"""
+Tipos de dados
+str - string
+int- inteiros
+float - real
+bool - booleano/lógico
+"""
+
+"""
+print('Lucas', type('Lucas'), bool('Lucas'))  #str
+print(10, type(10))  #int
+print(20.23, type(20.23))  #float
+print(10 == 10, type(10 == 10))  #bool-true
+print('L' == 'l', type('L' == 'l'))  #bool-false
+"""
+
+# String - Nome
+print('Lucas Henrique', type('Lucas Henrique'))
+
 # Int - Idade
 print(19, type(19))
 
