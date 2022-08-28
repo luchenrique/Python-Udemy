3deb15aca48f8d4a367617bb30bb3e2701eca976
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-27 21:44:50 -0300

    Tipos de dados

---
+++ logic_prog/aula4/aula4.py
@@ -0,0 +1,27 @@
+"""
+Tipos de dados
+str - string
+int- inteiros
+float - real
+bool - booleano/lógico
+"""
+
+"""
+print('Lucas', type('Lucas'), bool('Lucas'))  #str
+print(10, type(10))  #int
+print(20.23, type(20.23))  #float
+print(10 == 10, type(10 == 10))  #bool-true
+print('L' == 'l', type('L' == 'l'))  #bool-false
+"""
+
+# String - Nome
+print('Lucas Henrique', type('Lucas Henrique'))
+
+# Int - Idade
+print(19, type(19))
+
+# Float - Altura
+print(1.83, type(1.83))
+
+# Bool - Maior de idade
+print(19 >= 18, type(19 >= 18))
