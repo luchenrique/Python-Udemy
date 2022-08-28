0e82d4948ef4672d02a7b0001c4fd90c15cc46b7
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-27 22:25:15 -0300

    ...

---
+++ logic_prog/aula5/aula5.py
@@ -0,0 +1,16 @@
+"""
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+// Divisão inteira 
+** Potenciação
+% Módulo - resto de uma divisão
+() Alterar a precedência 
+"""
+
+print('Adição:', 10 + 10)
+print('Subtração:', 10 - 5)
+print('Multiplicação:', 10 * 10)
+print('Divisão: ', 10 / 2)
+print(10 + 10)
