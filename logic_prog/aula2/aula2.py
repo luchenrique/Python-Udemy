60643d8a92ca2edcbe7e4d3e28dd316a0544e39e
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-27 21:26:35 -0300

    Utilizando separador

---
+++ logic_prog/aula2/aula2.py
@@ -0,0 +1,17 @@
+# print(123456)
+# print('Lucas', 'Henrique')
+
+"""
+print('Lucas', 'Henrique', sep='-', end='###')
+print('Joao', 'e', 'Maria', sep='-', end='')
+"""
+
+"""
+print('824', '176', '070', sep='.', end='-')
+print('18')
+"""
+
+### Pergunta 1: Qual a saída do comando abaixo:
+
+print('Estou', 'aprendendo', 'Python', sep='-', end=' ')
+print('isso é muito', 'legal', sep='-', end='')
