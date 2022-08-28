5623e52e302ecd1fefd46b59ae25fd406970a219
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-27 21:27:22 -0300

    Strings

---
+++ logic_prog/aula3/aula3.py
@@ -0,0 +1,10 @@
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Essa é uma \"string\" (str).")
+print('Essa é uma \'string\' (str).')
+print('Essa é uma \n (str).') ### \n quebra linha
+print(r'Essa é uma \n (str).') ### r ignora comandos dentro do texto
