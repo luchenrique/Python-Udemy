d934e34ec8dd21fe3e6704fb71ff25fcffff5932
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-28 16:29:23 -0300

    Variáveis

---
+++ logic_prog/aula6/aula6.py
@@ -0,0 +1,24 @@
+# Variáveis
+"""
+Variaveis podem conter números porém não podem começar com um números 
+Fazer separações com _
+Utilizar letas minúsculas
+"""
+
+nome = 'Lucas'
+idade = 19
+altura = 1.83
+e_maior = idade > 18
+peso = 70
+imc = peso // (altura**2)
+
+
+""""
+print('Nome:', nome, type(nome))
+print('Idade', idade, type(idade))
+print('Altura', altura, type(altura))
+print('É maior', e_maior, type(e_maior))
+"""
+
+print(nome, 'tem', idade, 'anos de idade e', altura, 'de altura')
+print('Seu indice de massa corportal é de', imc)
