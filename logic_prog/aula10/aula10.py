418c203bf1226e1501c638602893e0690254136e
Seu Nome <lucas.barbosa3680@gmail.com>
2022-08-31 15:58:59 -0300

    IF, ELIF e ELSE + Booleans

--- logic_prog/aula10/aula10.py
+++ logic_prog/aula10/aula10.py
@@ -4,3 +4,11 @@ Condições IF, ELIF e ELSE
 
 #  round() - arredondar
 
+if False:
+    print('Verdadeiro')
+elif False:
+    print('Agora é verdadeiro')
+elif False:
+    print('Mais uma verdadeira')
+else:
+    print('Não é verdadeira')
