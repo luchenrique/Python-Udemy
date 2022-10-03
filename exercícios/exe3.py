76bbd31991902754bbf27a2dcdbe3bf796fa62fb
Seu Nome <lucas.barbosa3680@gmail.com>
2022-10-03 15:35:33 -0300

    tabuada

---
+++ exercícios/exe3.py
@@ -0,0 +1,13 @@
+x = int(input('Digite um número para saber a tabuada: '))
+cont = 0
+
+print('')
+print('*' * 30)
+while cont <= 10:
+    print(f'{x} X {cont} = {x*cont}')
+    cont += 1
+else:
+    print('*' * 30)
+    print('')
+    print('Tabuada Finalizada')
+    print('')
